daa97a8d16442688ce3230eb282f8fff064bc8ed
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-03-02 19:30:56 +0000

    added continuous delivery 10

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
+echo "deploy start"
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 pm2 stop example_app
 cd ExampleApplication/
 npm install
 pm2 start ./bin/www --name example_app
+echo "deploy done"
