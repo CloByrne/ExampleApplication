65f9e8433384b96de8a3a63f95241dffff56f046
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-03-02 19:55:58 +0000

    added continuous delivery 16

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ sudo npm install -g pm2
 pm2 stop example_app
 
 # change directory into folder where application is downloaded
+sudo chmod 777 ExampleApplication/
 cd ExampleApplication/
 
 # Install application dependancies
